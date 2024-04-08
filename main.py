b086cef45ec8ab43a06e78e894acc4e44068062a
Deepthi306 <112242134+Deepthi306@users.noreply.github.com>
2024-04-08 21:42:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ import psycopg2
 
 conn = psycopg2.connect(
     dbname="sample_db",
-    user="app",
-    password="247E5Zb8p5uQ1Ca89rPxld9k",
-    host="informally-sought-honeybee.a1.pgedge.io",
+    user="",
+    password="",
+    host="",
     port="5432"
 )
 
